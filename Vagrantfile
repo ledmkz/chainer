796e1853202c6f7ccbb9cf4debab05a517bba591
Akito Shibue <shibue@mtl.t.u-tokyo.ac.jp>
2016-10-19 15:04:46 +0900

    test

--- Vagrantfile
+++ Vagrantfile
@@ -22,8 +22,6 @@ Vagrant.configure(2) do |config|
   end
   config.vm.synced_folder "sync", "/home/vagrant/sync", create:true, type:"rsync"
 
-
-
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
   # `vagrant box outdated`. This is not recommended.
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,27 @@
 test -f /etc/bootstrapped && exit
 
 apt update
-apt install -y git g++ make emacs language-pack-ja-base language-pack-ja gdb python-pip ubuntu-desktop aptitude
+apt install -y git g++ make emacs language-pack-ja-base language-pack-ja gdb python-pip ubuntu-desktop aptitude kpartx parted libyaml-devel
 update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 
 aptitude install -y libpython-dev
 pip install -U numpy
 pip install chainer
+
+#setup bridge
+sed -i -e 's/exit 0//g' /etc/rc.local
+echo "ifconfig eth0 down" >> /etc/rc.local
+echo "ifconfig eth0 up" >> /etc/rc.local
+echo "ip addr flush dev eth0" >> /etc/rc.local
+echo "brctl addbr br0" >> /etc/rc.local
+echo "brctl stp br0 off" >> /etc/rc.local
+echo "brctl setfd br0 0" >> /etc/rc.local
+echo "brctl addif br0 eth0" >> /etc/rc.local
+echo "brctl stp br0 off" >> /etc/rc.local
+echo "ifconfig br0 up" >> /etc/rc.local
+echo "dhclient br0 up" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+/etc/rc.local
 
 # install pyenv
 cd /usr/local
@@ -22,3 +37,4 @@ source /etc/profile.d/pyenv.sh
 pyenv install -v 3.5.2
 pyenv global 3.5.2
 
+date > /etc/bootstrapped
