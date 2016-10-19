00c5369b84481b45bf278555a3b2abf316b1f47d
Akito Shibue <shibue@mtl.t.u-tokyo.ac.jp>
2016-10-19 14:04:31 +0900

    use ubuntu-desktop

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 test -f /etc/bootstrapped && exit
 
 apt update
-apt install -y git g++ make emacs language-pack-ja-base language-pack-ja gdb
+apt install -y git g++ make emacs language-pack-ja-base language-pack-ja gdb python-pip ubuntu-desktop
 update-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 
